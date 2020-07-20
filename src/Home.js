587501c50d1999cb349af220d1a40ff1d3d98c98
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-07-20 00:38:37 -0300

    Create Drawer

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,31 @@
 import React from 'react';
-import { makeStyles, AppBar, Toolbar, IconButton, Button } from '@material-ui/core';
-import { Menu, AccountCircle, VideoCall, MoreVert, Apps} from '@material-ui/icons';
+import { 
+  makeStyles,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  Box,
+  Typography
+} from '@material-ui/core';
+import {
+  Menu,
+  AccountCircle,
+  VideoCall,
+  MoreVert,
+  Apps,
+  Home as HomeIcon,
+  Subscriptions,
+  Whatshot,
+  VideoLibrary,
+  History
+} from '@material-ui/icons';
 
 import whiteLogo from './assets/images/white-logo.png';
 import blackLogo from './assets/images/black-logo.png';
@@ -10,10 +35,19 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh'
   },
   appBar: {
-    boxShadow: 'none'
+    boxShadow: 'none',
+    zIndex: theme.zIndex.drawer + 1,
   },
   logo: {
     height: 27
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+    borderRight: 'none'
   },
   menuIcon: {
     paddingRight: theme.spacing(5),
@@ -24,6 +58,13 @@ const useStyles = makeStyles((theme) => ({
   },
   grow: {
     flexGrow: 1
+  },
+  listItemText: {
+    fontSize: 14
+  },
+  listItem: {
+    paddingTop: 4,
+    paddingBottom: 4,
   }
 }));
 
@@ -75,6 +116,83 @@ export default function Home() {
           </Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            <ListItem button classes={{ root: classes.listItem }}>
+              <ListItemIcon>{<HomeIcon />}</ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.listItemText
+                }}
+                primary={'Inicio'}
+              />
+            </ListItem>
+            <ListItem button classes={{ root: classes.listItem }}>
+              <ListItemIcon>{<Whatshot />}</ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.listItemText
+                }}
+                primary={'Em alta'}
+              />
+            </ListItem>
+            <ListItem button classes={{ root: classes.listItem }}>
+              <ListItemIcon>{<Subscriptions />}</ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.listItemText
+                }}
+                primary={'Inscrições'}
+              />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItem button classes={{ root: classes.listItem }}>
+              <ListItemIcon>{<VideoLibrary />}</ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.listItemText
+                }}
+                primary={'Biblioteca'}
+              />
+            </ListItem>
+            <ListItem button classes={{ root: classes.listItem }}>
+              <ListItemIcon>{<History />}</ListItemIcon>
+              <ListItemText
+                classes={{
+                  primary: classes.listItemText
+                }}
+                primary={'Histórico'}
+              />
+            </ListItem>
+          </List>
+          <Divider />
+          <Box p={8}>
+            <Typography variant='body2'>
+              Faça login para curtir vídeos, comentar e se inscrever.
+            </Typography>
+            <Box mt={2}>
+              <Button
+                variant='outlined'
+                color='secondary'
+                startIcon={<AccountCircle />}
+              >
+                Fazer Login
+              </Button>
+            </Box>
+          </Box>
+          <Divider />
+        </div>
+      </Drawer>
     </div>
   )
 }
